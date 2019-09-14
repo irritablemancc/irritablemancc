aeb3c27f869d2dd6df44dac209b1c9cb4681875e
irritablemancc <2414272275@qq.com>
2019-09-14 21:01:07 +0800

    尝试

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ class BlockHandler(tornado.web.RequestHandler):
         title = '草'
         content = '''
             s十年生死两茫茫，不思量自难忘，千里孤坟，无处话凄凉，纵使相逢应不识，尘满面，鬓如霜。夜来幽梦忽还乡，小轩窗，正梳妆，相顾无言，惟有泪千行。料得年年断肠处，明月夜短松冈。
+    。书上说，天下没有不散的宴席，书上还说，人生何处不相逢。
         '''
         self.render('article.html', title=title, content=content)
 
